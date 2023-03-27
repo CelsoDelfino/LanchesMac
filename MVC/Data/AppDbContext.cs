6705125afb8a550192d53acdda679b1b04eb0fd8
Celso Delfino <celso.delfino@capgemini.com>
2023-03-27 10:34:59 -0300

    subindo porj

---
+++ MVC/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using LanchesMac.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LanchesMac.Data
+{
+    public class AppDbContext : DbContext
+    {
+        //carrega as informações de configuração necessárias para configurar o DbContext
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options) {}
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Lanche> Lanches { get; set; }
+
+    }
+}
---
+++ MVC/Repository/CategoriaRepository.cs
@@ -0,0 +1,17 @@
+﻿using LanchesMac.Data;
+using LanchesMac.Models;
+using LanchesMac.Repository.Interfaces;
+
+namespace LanchesMac.Repository
+{
+    public class CategoriaRepository : ICategoriaRepository
+    {
+        private readonly AppDbContext _context;
+        public CategoriaRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Categoria> Categorias => _context.Categorias.ToList();
+    }
+}
